1872d5f99fcfc3e5ea50d3f62d1864dbb166f6c0
Anil SENOCAK <asenocak@netas.com.tr>
2019-01-10 10:29:07 +0300

    articles done

--- index.py
+++ index.py
@@ -26,20 +26,28 @@ app.config["MYSQL_CURSORCLASS"] =   "DictCursor"
 mysql = MySQL(app)
 
 @app.route("/")
+@app.route("/articles")
+@app.route("/article")
 def index():
-    return render_template("index.html")
-
-
-@app.route("/hakkımda")
-def hakkımda():
-    return render_template("hakkımda.html")
-
-
-@app.route("/article/<string:id>")
-def article(id):
     cursor = mysql.connection.cursor()
-    sorgu = "select * from articles where id = %s"
-    result = cursor.execute(sorgu,(id,))
+    sorgu = "select * from articles order by created_at desc"
+    result = cursor.execute(sorgu)
+    if result > 0:
+        articles = cursor.fetchall()
+        return render_template("index.html",articles = articles)
+    else:
+        return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/article/<string:url>")
+def article(url):
+    cursor = mysql.connection.cursor()
+    sorgu = "select * from articles where url = %s"
+    result = cursor.execute(sorgu,(url,))
     if result > 0:
         article = cursor.fetchone()
         return render_template("article.html",article = article) 
@@ -121,11 +129,12 @@ def logout():
 
 @app.route("/admin")
 @app.route("/admin/articles")
+@app.route("/admin/article")
 @login_required
 def admin():
     cursor = mysql.connection.cursor()
-    sorgu = "select * from articles where author = %s"
-    result = cursor.execute(sorgu,(session["username"],))
+    sorgu = "select * from articles order by created_at desc"
+    result = cursor.execute(sorgu)
     if result > 0 :
         articles = cursor.fetchall()
         return render_template("admin.html",articles = articles)
@@ -147,8 +156,8 @@ def addarticle():
             title = form.title.data
             content = form.content.data
             cursor = mysql.connection.cursor()
-            sorgu = "Insert  into articles(title,content,author) values(%s,%s,%s)"
-            cursor.execute(sorgu,(title,content,session["username"]))
+            sorgu = "Insert  into articles(title,content,author,url) values(%s,%s,%s,%s)"
+            cursor.execute(sorgu,(title,content,session["username"],self_url(title)))
             mysql.connection.commit()
             cursor.close()
             flash('Başarıyla Makale Eklediniz.',"success")
@@ -157,6 +166,73 @@ def addarticle():
             return render_template("addarticle.html",form=form)
     else:
         return redirect(url_for("index"))
+
+
+@app.route("/admin/article/<string:id>/edit",methods=["GET","POST"])
+@login_required
+def edit_article(id):
+    form = ArticleForm(request.form)
+    if request.method == "POST" and form.validate():
+        title = form.title.data
+        content = form.content.data
+        cursor = mysql.connection.cursor()
+        sorgu = "update articles set title=%s, content=%s, author=%s, url=%s where id = %s"
+        cursor.execute(sorgu,(title,content,session["username"],self_url(title),id))
+        mysql.connection.commit()
+        cursor.close()
+        flash('Başarıyla Makale Güncellendi.',"success")
+        return redirect(url_for("admin"))
+    else:  
+        cursor = mysql.connection.cursor()
+        sorgu = "select * from articles where id = %s"
+        result = cursor.execute(sorgu,(id,))
+        if result > 0:
+            article = cursor.fetchone()
+            form = ArticleForm()
+            form.title.data = article["title"]
+            form.content.data = article["content"]
+            return render_template("editarticle.html",form=form) 
+        else:
+            return redirect(url_for("index"))
+
+
+@app.route("/admin/article/<string:id>/delete",methods=["GET","POST"])
+@login_required
+def delete_article(id):
+    form = ArticleForm(request.form)
+    if request.method == "POST":
+        cursor = mysql.connection.cursor()
+        sorgu = "delete from articles where id = %s"
+        cursor.execute(sorgu,(id,))
+        mysql.connection.commit()
+        cursor.close()
+        flash('Başarıyla Makale Silindi.',"success")
+        return redirect(url_for("admin"))
+    else:  
+        return render_template("deletearticle.html",form=form) 
+
+
+def self_url(string):
+    string = string.lower()
+    string = string.replace(' ', '_')
+    string = string.replace('?', '_')
+    string = string.replace('%', '_')
+    string = string.replace('&', '_')
+    string = string.replace('.', '_')
+    string = string.replace(',', '_')
+    string = string.replace('!', '_')
+    string = string.replace('ı', 'i')
+    string = string.replace('ğ', 'g')
+    string = string.replace('Ğ', 'g')
+    string = string.replace('ş', 's')
+    string = string.replace('Ş', 's')
+    string = string.replace('Ö', 'o')
+    string = string.replace('ö', 'o')
+    string = string.replace('Ç', 'c')
+    string = string.replace('ç', 'c')
+    string = string.replace('Ü', 'u')
+    string = string.replace('ü', 'u')
+    return string
 
 if __name__ == "__main__":
     app.run(debug=True)
